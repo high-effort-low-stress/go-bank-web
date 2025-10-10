2c14a09c5168e72f1711b2ab5fc9860efb1b0c9f
iago silva <iago.scandido3@gmail.com>
2025-10-10 17:02:18 -0300

    major changes: create a cpf card for input cpf and then redirect to the main create account form
    minor changes: change tag anchor to Link tag of nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { useState } from "react";
+import { AccountDialog } from "@/components/AccountDialog";
 import { Features } from "@/components/Features";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
@@ -6,15 +8,20 @@ import { Hero } from "@/components/Hero";
 import { SecuritySection } from "@/components/SecuritySection";
 
 export default function landingPage() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-1">
         <Hero />
+
         <Features />
         <SecuritySection />
       </main>
       <Footer />
+
+      <AccountDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
     </div>
   );
 }
