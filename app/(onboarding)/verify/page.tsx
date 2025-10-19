05d5083ccd5f07b839bb219e7bd90a69ee42406d
iago silva <iago.scandido3@gmail.com>
2025-10-19 00:03:56 -0300

    chore: general improvement on unit tests , refact on actions and schema

--- app/(onboarding)/verify/page.tsx
+++ app/(onboarding)/verify/page.tsx
@@ -1,6 +1,6 @@
+import { onboardingVerifyAction } from "@/actions/onboarding/verifyAction";
 import { OnboardingUnverified } from "@/components/onboarding/verify/onboarding-unverified";
 import OnboardingVerified from "@/components/onboarding/verify/onboarding-verified";
-import { onboardingVerifyAction } from "@/lib/actions/onboarding/verifyAction";
 
 type VerifyPageProps = {
   searchParams: Promise<{ token?: string }>;
@@ -9,7 +9,7 @@ type VerifyPageProps = {
 const VerifyPage = async ({ searchParams }: VerifyPageProps) => {
   const { token } = await searchParams;
 
-  const verify = await onboardingVerifyAction(token);
+  const verify = await onboardingVerifyAction({ token });
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-muted/30">
