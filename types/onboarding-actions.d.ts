0e29d2a0f25850b49672addd490052b1bbdeb4e1
iago silva <iago.scandido3@gmail.com>
2025-10-19 00:03:56 -0300

    chore: general improvement on unit tests , refact on actions and schema

---
+++ types/onboarding-actions.d.ts
@@ -0,0 +1,4 @@
+export type OnboardingResponse = {
+  success: boolean;
+  description: string;
+};
--- types/register.d.ts
+++
@@ -1,5 +0,0 @@
-export interface OnboardingCreateUserStart {
-  fullName: string;
-  document: string;
-  email: string;
-}
